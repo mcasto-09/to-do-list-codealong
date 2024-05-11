736d57f0e928746409ed4ac93161e3fd2819148e
Mary Castorani <mgcastorani@gmail.com>
2024-05-11 15:43:38 -0600

    step two complete

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+function removeTodo(event) {
+    event.target.parentNode.remove();
+}
+
 function addTodo(event) {
     event.preventDefault();
     const item = document.createElement("li");
     item.innerText = document.querySelector("#item").value;
+    
+    let button = document.createElement("button");
+    button.innerText = "click me"
+    button.addEventListener("click", removeTodo);
+    item.append(button);
+
+
     const list = document.querySelector("ul");
     list.appendChild(item);
   }
