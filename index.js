549d5ab62c0b6369ca54c36ac79abe432a9a530b
Mary Castorani <mgcastorani@gmail.com>
2024-05-11 16:05:14 -0600

    done

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 function removeTodo(event) {
     event.target.parentNode.remove();
 }
+function completeTodo (event) {
+    let value = event.target.getAttribute('aria-checked');
+    if (value !== 'true') {
+        event.target.setAttribute('aria-checked', 'true');
+      } else {
+        event.target.setAttribute('aria-checked', 'false');
+      }
 
+}
 function addTodo(event) {
     event.preventDefault();
     const item = document.createElement("li");
     item.innerText = document.querySelector("#item").value;
+    item.addEventListener('click', completeTodo)
     
     let button = document.createElement("button");
-    button.innerText = "click me"
+    button.innerText = "complete"
     button.addEventListener("click", removeTodo);
     item.append(button);
 
